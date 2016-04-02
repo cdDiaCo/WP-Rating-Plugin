2cf6705e9e245268197aa3779ff97bb4bffaa918
cdDiaCo <claudia_cocioaba@yahoo.ro>
2016-04-02 21:35:53 +0300

    test

--- ajax_rating.js
+++ ajax_rating.js
@@ -38,6 +38,10 @@ jQuery(document).ready(function($) {
         });
     }
 
+    function testFunction() {
+        
+    }
+
 
     $('.dashicons-thumbs-up, .dashicons-thumbs-down').on('click', function() {
         var selectedRating;
